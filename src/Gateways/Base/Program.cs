f199616d02b7ff30d704626532b8a32a73c19619
kkadir <kadir.kilicoglu@aurea.com>
2019-09-12 01:10:17 +0300

    Setup a basic Ocelot API Gateway.

---
+++ src/Gateways/Base/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace UserLocation.Gateways.Base
+{
+    using System.IO;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args);
+
+            builder
+                .ConfigureServices(services => services.AddSingleton(builder))
+                .ConfigureAppConfiguration(configuration => configuration
+                    .AddJsonFile("ocelot.json"))
+                .UseStartup<Startup>();
+
+            return builder;
+        }
+    }
+}
